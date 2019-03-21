0800b8c894e3015ae8946f5a203e71d9620db7c6
Zhenya Mamenko <emamenko@student.42.us.org>
2019-03-21 14:25:32 -0700

    processes

---
+++ processes.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   processes.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emamenko <emamenko@student.42.us.org>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/21 14:04:06 by emamenko          #+#    #+#             */
+/*   Updated: 2019/03/21 14:05:14 by emamenko         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PROCESSES_H
+# define PROCESSES_H
+
+# include <stdlib.h>
+
+void	process_md5(size_t f, int cnt, char **av);
+
+#endif
